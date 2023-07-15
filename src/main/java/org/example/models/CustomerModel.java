63c2e16042aadb31a4330ce1eb37344fbe81aff3
Katrin Valge <katrin.valge@mail.ee>
2023-07-15 21:29:47 +0300

    Created CustomerModel.

---
+++ src/main/java/org/example/models/CustomerModel.java
@@ -0,0 +1,41 @@
+package org.example.models;
+
+public class CustomerModel {
+
+    private String fullName;
+    private String registrationCode;
+    private String email;
+    private String telephone;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRegistrationCode() {
+        return registrationCode;
+    }
+
+    public void setRegistrationCode(String registrationCode) {
+        this.registrationCode = registrationCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
