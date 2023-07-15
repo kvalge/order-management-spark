d26c8d849b3ec72a860fdb1d85e0b86c39447907
Katrin Valge <katrin.valge@mail.ee>
2023-07-15 21:15:59 +0300

    Revised gradle dependencies. Created customer.hbs. Created method to get request from /customer.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,17 @@ repositories {
 }
 
 dependencies {
+    implementation ("com.sparkjava:spark-core:2.9.3")
+    implementation ("com.sparkjava:spark-template-handlebars:2.7.1")
+
+    implementation ("org.slf4j:slf4j-api:1.7.36")
+    implementation ("org.slf4j:slf4j-jdk14:1.7.36")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import spark.ModelAndView;
+
+import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        port(8080);
+
+        get("/customer", (request, response) -> {
+            return new ModelAndView(null, "customer.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
