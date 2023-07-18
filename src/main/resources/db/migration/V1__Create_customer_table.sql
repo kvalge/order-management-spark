43d92c8f171ecf824471d8c247a97cf5f058f419
Katrin Valge <katrin.valge@mail.ee>
2023-07-18 21:34:42 +0300

    Revised column names.

--- src/main/resources/db/migration/V1__Create_customer_table.sql
+++ src/main/resources/db/migration/V1__Create_customer_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE customer
 (
     id               BIGSERIAL primary key not null,
-    fullName         VARCHAR(250),
-    registrationCode VARCHAR(500),
+    full_name         VARCHAR(250),
+    registration_code VARCHAR(500),
     email            VARCHAR(50),
     telephone        VARCHAR(50)
 );
