f4c502500f3a4430437f8f5fdf31ddd0a61ac717
Katrin Valge <katrin.valge@mail.ee>
2023-07-28 20:36:21 +0300

    Created order_line model.

---
+++ src/main/java/org/example/model/entity_models/OrderLineModel.java
@@ -0,0 +1,33 @@
+package org.example.model.entity_models;
+
+public class OrderLineModel {
+
+    public static final String TABLE_NAME = "order_line";
+    private Long id;
+    private Long customerOrderId;
+    private Long productId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerOrderId() {
+        return customerOrderId;
+    }
+
+    public void setCustomerOrderId(Long customerOrderId) {
+        this.customerOrderId = customerOrderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
