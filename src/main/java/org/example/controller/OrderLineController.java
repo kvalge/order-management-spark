9b5effadab8cbad4aefd470365b49fbc5e294ae4
Katrin Valge <katrin.valge@mail.ee>
2023-08-03 17:31:47 +0300

    Revised order line to display product name and price.

--- src/main/java/org/example/controller/OrderLineController.java
+++ src/main/java/org/example/controller/OrderLineController.java
@@ -1,7 +1,9 @@
 package org.example.controller;
 
 import org.example.model.entity_models.OrderLineModel;
+import org.example.model.view_models.ProductViewModel;
 import org.example.service.OrderLineService;
+import org.example.service.ProductService;
 import spark.Request;
 import spark.Response;
 
@@ -13,6 +15,7 @@ import static java.lang.Long.parseLong;
 public class OrderLineController extends Controller{
 
     private final OrderLineService orderLineService = new OrderLineService();
+    private final ProductService productService = new ProductService();
 
     /**
      * Inserts new order line with selected product and order id.
@@ -27,8 +30,10 @@ public class OrderLineController extends Controller{
         orderLineModel.setProductId(id);
         orderLineService.insert(orderLineModel);
 
+        ProductViewModel product = productService.getById(request);
+
         Map<String, Object> model = new HashMap<>();
-        model.put("order_line", orderLineModel);
+        model.put("product", product);
 
         return render("order_line/order_line.hbs", model);
     }
